e2a0cafbc92d509f6648722dfae66a991baf0810
JadirJunior <jadirjunior8@gmail.com>
2024-10-13 16:11:16 +0100

    feat: Integration tests - validate createdAt field on create user

--- src/users/domain/entities/__tests__/integration/user.entity-int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity-int.spec.ts
@@ -99,5 +99,11 @@ describe('UserEntity Integration Tests', () => {
       }
       expect(() => new UserEntity(props)).toThrow(EntityValidationError)
     })
+
+    it('Should a valid user', () => {
+      expect.assertions(0)
+      const props: UserProps = UserDataBuilder({})
+      new UserEntity(props)
+    })
   })
 })
