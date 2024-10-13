ca72fe10af81c755154084f9b68cf153d9661e12
JadirJunior <jadirjunior8@gmail.com>
2024-10-13 16:57:52 +0100

    feat: InMemoryRepository unit tests - insert method

---
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -0,0 +1,25 @@
+import { Entity } from '@/shared/domain/entities/entity'
+import { InMemoryRepository } from '../../in-memory.repository'
+
+type StubEntityProps = {
+  name: string
+  price: number
+}
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemoryRepository extends InMemoryRepository<StubEntity> {}
+
+describe('InMemoryRepository Unit Tests', () => {
+  let sut: StubInMemoryRepository
+
+  beforeEach(() => {
+    sut = new StubInMemoryRepository()
+  })
+
+  it('Should inserts a new entity', async () => {
+    const entity = new StubEntity({ name: 'any_name', price: 10 })
+    await sut.insert(entity)
+    expect(entity.toJson()).toStrictEqual(sut.items[0].toJson())
+  })
+})
