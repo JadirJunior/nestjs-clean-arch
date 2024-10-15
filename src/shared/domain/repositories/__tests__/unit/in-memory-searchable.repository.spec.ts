449afcfffdbbf271a594fe897b84b1e79344050e
JadirJunior <jadirjunior8@gmail.com>
2024-10-15 14:42:09 +0100

    feat: Unit tests InMemorySearchable class

---
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -0,0 +1,38 @@
+import { Entity } from '@/shared/domain/entities/entity'
+import { InMemorySearchableRepository } from '../../in-memory-searchable.repository '
+
+type StubEntityProps = {
+  name: string
+  price: number
+}
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemorySearchableRepository extends InMemorySearchableRepository<StubEntity> {
+  sortableFields: string[] = ['name', 'price']
+  protected async applyFilter(
+    items: StubEntity[],
+    filter: string | null,
+  ): Promise<StubEntity[]> {
+    if (!filter) return items
+
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+}
+
+describe('InMemoryRepository Unit Tests', () => {
+  let sut: StubInMemorySearchableRepository
+
+  beforeEach(() => {
+    sut = new StubInMemorySearchableRepository()
+  })
+
+  describe('applyFilter method', () => {
+    it('', () => {})
+  })
+  describe('applySort method', () => {})
+  describe('applyPaginate method', () => {})
+  describe('search method', () => {})
+})
