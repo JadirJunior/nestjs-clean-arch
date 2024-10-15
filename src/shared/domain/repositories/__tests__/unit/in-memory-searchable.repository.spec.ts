dca8db85c1e2e014f1b0ac2b191ba6e7879df44a
JadirJunior <jadirjunior8@gmail.com>
2024-10-15 23:07:44 +0100

    feat: InMemorySearchableRepository unit tests - applyPaginate

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -99,6 +99,28 @@ describe('InMemoryRepository Unit Tests', () => {
       expect(itemsSorted).toStrictEqual([items[2], items[0], items[1]])
     })
   })
-  describe('applyPaginate method', () => {})
+  describe('applyPaginate method', () => {
+    it('should paginate items', async () => {
+      const items = [
+        new StubEntity({ name: 'a', price: 50 }),
+        new StubEntity({ name: 'b', price: 100 }),
+        new StubEntity({ name: 'c', price: 30 }),
+        new StubEntity({ name: 'd', price: 30 }),
+        new StubEntity({ name: 'e', price: 30 }),
+      ]
+
+      let itemsPaginated = await sut['applyPaginate'](items, 1, 2)
+      expect(itemsPaginated).toStrictEqual([items[0], items[1]])
+
+      itemsPaginated = await sut['applyPaginate'](items, 2, 2)
+      expect(itemsPaginated).toStrictEqual([items[2], items[3]])
+
+      itemsPaginated = await sut['applyPaginate'](items, 3, 2)
+      expect(itemsPaginated).toStrictEqual([items[4]])
+
+      itemsPaginated = await sut['applyPaginate'](items, 4, 2)
+      expect(itemsPaginated).toStrictEqual([])
+    })
+  })
   describe('search method', () => {})
 })
