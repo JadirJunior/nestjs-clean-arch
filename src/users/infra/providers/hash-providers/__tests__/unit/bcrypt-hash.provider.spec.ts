36b9f326f496be1f061065e7ed124ce337047243
JadirJunior <jadirjunior8@gmail.com>
2024-10-19 12:57:57 +0100

    feat: BcryptHashProvider unit tests

---
+++ src/users/infra/providers/hash-providers/__tests__/unit/bcrypt-hash.provider.spec.ts
@@ -0,0 +1,33 @@
+import { BcryptHashProvider } from '../../bcrypt-hash.provider'
+
+describe('UserInMemoryRepository Unit Tests', () => {
+  let sut: BcryptHashProvider
+
+  beforeEach(() => {
+    sut = new BcryptHashProvider()
+  })
+
+  it('Should return encrypted password', async () => {
+    const password = 'TestPassword123'
+    const hash = await sut.generateHash(password)
+    expect(hash).toBeDefined()
+  })
+
+  it('Should return false on invalid password and hash compare', async () => {
+    const password = 'TestPassword123'
+    const hash = await sut.generateHash(password)
+
+    const result = await sut.compareHash('InvalidPassword', hash)
+
+    expect(result).toBeFalsy()
+  })
+
+  it('Should return true on invalid password and hash compare', async () => {
+    const password = 'TestPassword123'
+    const hash = await sut.generateHash(password)
+
+    const result = await sut.compareHash(password, hash)
+
+    expect(result).toBeTruthy()
+  })
+})
