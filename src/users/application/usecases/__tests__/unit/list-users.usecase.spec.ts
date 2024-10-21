a4387c2a8281ba9b909d42e94199285fc5b93fd2
JadirJunior <jadirjunior8@gmail.com>
2024-10-21 15:15:30 +0100

    feat: ListUsersUseCase unit tests - execute method with pagination, filter and sort

--- src/users/application/usecases/__tests__/unit/list-users.usecase.spec.ts
+++ src/users/application/usecases/__tests__/unit/list-users.usecase.spec.ts
@@ -14,6 +14,7 @@ describe('ListUsersUseCase unit tests', () => {
   })
 
   it('toOutput method', () => {
+    const entity = new UserEntity(UserDataBuilder({}))
     let result = new UserRepository.SearchResult({
       items: [],
       total: 1,
@@ -34,8 +35,6 @@ describe('ListUsersUseCase unit tests', () => {
       perPage: 2,
     })
 
-    const entity = new UserEntity(UserDataBuilder({}))
-
     result = new UserRepository.SearchResult({
       items: [entity],
       total: 1,
@@ -53,6 +52,57 @@ describe('ListUsersUseCase unit tests', () => {
       total: 1,
       currentPage: 1,
       lastPage: 1,
+      perPage: 2,
+    })
+  })
+
+  it('should return the users ordered by createdAt', async () => {
+    const createdAt = new Date()
+    const items = [
+      new UserEntity(UserDataBuilder({ createdAt })),
+      new UserEntity(
+        UserDataBuilder({ createdAt: new Date(createdAt.getTime() + 1) }),
+      ),
+      new UserEntity(
+        UserDataBuilder({ createdAt: new Date(createdAt.getTime() + 2) }),
+      ),
+    ]
+
+    repository.items = items
+    const output = await sut.execute({})
+
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map(item => item.toJson()),
+      total: 3,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 15,
+    })
+  })
+
+  it('should return the users using pagination, sort and filter', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'a' })),
+      new UserEntity(UserDataBuilder({ name: 'AA' })),
+      new UserEntity(UserDataBuilder({ name: 'Aa' })),
+      new UserEntity(UserDataBuilder({ name: 'b' })),
+      new UserEntity(UserDataBuilder({ name: 'c' })),
+    ]
+
+    repository.items = items
+    const output = await sut.execute({
+      page: 1,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'a',
+    })
+
+    expect(output).toStrictEqual({
+      items: [items[1].toJson(), items[2].toJson()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 2,
       perPage: 2,
     })
   })
