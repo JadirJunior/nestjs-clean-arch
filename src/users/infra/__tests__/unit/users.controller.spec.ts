9eec0f88d187405f4a33aed068578b5ab074a58b
JadirJunior <jadirjunior8@gmail.com>
2024-10-30 20:00:49 +0000

    feat: UsersController unit tests - create method

---
+++ src/users/infra/__tests__/unit/users.controller.spec.ts
@@ -0,0 +1,46 @@
+import { SignupUseCase } from '@/users/application/usecases/signup.usecase'
+import { UsersController } from '../../users.controller'
+import { UserOutput } from '@/users/application/dto/user-output'
+import { SignupDto } from '../../dtos/signup.dto'
+
+describe('UsersController unit tests', () => {
+  let sut: UsersController
+  let id: string
+  let props: UserOutput
+
+  beforeEach(async () => {
+    sut = new UsersController()
+    id = '4c4360b9-8533-4c50-83fe-340fecc247b4'
+    props = {
+      id,
+      name: 'John Doe',
+      email: 'a@a.com',
+      password: '1234',
+      createdAt: new Date(),
+    }
+  })
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined()
+  })
+
+  it('should create a user', async () => {
+    const output: SignupUseCase.Output = props
+
+    const mockSignupUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+
+    sut['signupUseCase'] = mockSignupUseCase as any
+
+    const input: SignupDto = {
+      name: 'John Doe',
+      email: 'a@a.com',
+      password: '1234',
+    }
+
+    const result = await sut.create(input)
+    expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input)
+    expect(output).toMatchObject(result)
+  })
+})
